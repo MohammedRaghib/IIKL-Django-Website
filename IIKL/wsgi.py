3d2cb6514abfb5e89e206c66a41cd2a4c62064af
MohammedRaghib <mohammedmraghib@gmail.com>
2025-06-13 23:19:08 +0300

    replaced tijarat with IIKL

--- IIKL/wsgi.py
+++ IIKL/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Tijarat project.
+WSGI config for IIKL project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tijarat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IIKL.settings')
 
 application = get_wsgi_application()
